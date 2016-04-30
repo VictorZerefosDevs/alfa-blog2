e20d5b83f08ad99f933a7ef492e4822e18d9e1ed
Victor Zerefos <zvelopswebsite@gmail.com>
2016-04-30 15:50:09 -0300

    edit, show action created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,14 @@ class ArticlesController < ApplicationController
 
 	def new
 		@article = Article.new
+	end 
+
+	def edit
+		@article = Article.find(params[:id])
 	end
+
 	def create
 		@article = Article.new(article_params)
-		@article.save
 		if @article.save
 			flash[:notice] = "Article created"
 			redirect_to article_path(@article) 
@@ -17,9 +21,22 @@ class ArticlesController < ApplicationController
 		end	
 		
 	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
 
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def destroy
+		@article.destroy
 	end
 
 	private 
