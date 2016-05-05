16338f6de275b093dd04346cbfc3c2b7bbed0a5d
Victor Zerefos <zvelopswebsite@gmail.com>
2016-05-05 10:23:34 -0300

    created users articles views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update]
+	before_action :set_user, only: [:edit, :update, :show]
+	def index
+		@users = User.all
+	end
 
 	def new
 		@user = User.new
@@ -26,6 +29,10 @@ class UsersController < ApplicationController
 		else
 			render 'edit'
 		end
+	end
+
+	def show
+		
 	end
 
 	private
