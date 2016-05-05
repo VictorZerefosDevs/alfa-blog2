048f2e6158d5e45af022fa76a9610cae29fe73b0
Victor Zerefos <zvelopswebsite@gmail.com>
2016-05-05 11:00:41 -0300

    added pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
 	def index
-		@users = User.all
+		@users = User.paginate(page: params[:page], per_page: 3 )
 	end
 
 	def new
